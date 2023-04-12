e485d0cca69bd9df5cd240567438ce012becbdce
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-04-12 11:12:08 -0500

    se implementa el store para pokemon

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
 import App from './App'
-import './index.css'
+// import './index.css'
 
 import { store } from './store'
+import { PokemonApp } from './PokemonApp'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PokemonApp />
     </Provider>
   </React.StrictMode>,
 )
